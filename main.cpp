1e3259b2a1567beb7f4ef2672894c3e3065f04f2
Johnathan Hubrich <62731625+SiliconeHeartbeats@users.noreply.github.com>
2020-06-12 02:17:15 -0500

    added ability to read files

--- main.cpp
+++ main.cpp
@@ -1,12 +1,28 @@
 #include "FrequencyAnalyzer.h"
 
-ANFrequency Crypto;
+ANFrequency crypto;
 
 int main() {
     int input = 0;
-    Crypto.build();
-    Crypto.printfrequency();
+    int choice = 0;
+    std::cout << "(1)Enter text directly or (2)read from file?: ";
+    std::cin >> choice;
+    std::cin.ignore(256, '\n');
+    if (choice == 1) {
+        crypto.build();
+        crypto.printfrequency();
+    }
+    else if (choice == 2) {
+        crypto.scanbuild();
+        crypto.printfrequency();
+    }
+    else {
+        std::cout << "\nInvalid entry :c\n";
+        return 1;
+    }
+   
+    std::cout << "\nEnter any character to exit\n";
+    std::cout << crypto.whiteSpaceFreeInput.size() << "\n";
     std::cin >> input;
     return 0;
 }
-
